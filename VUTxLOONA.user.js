3dcdb82272cd3ea3f2201179f05d5876a1eb1c53
Kevin Lackó <kevinlacko.kl@gmail.com>
2022-04-04 13:54:59 +0200

    fitvut

--- VUTxLOONA.user.js
+++ VUTxLOONA.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         VUTxLOONA
 // @namespace    https://github.com/DEM0N194/VUTxLOONA
-// @version      1.1.5
+// @version      1.1.6
 // @description  r/place template script for VUT.CZ and r/LOONA
 // @author       u/DEM0N194
 // @match        https://www.reddit.com/r/place/*
@@ -27,6 +27,7 @@ const images = [
   [352, 1600, 1, "https://raw.githubusercontent.com/DEM0N194/VUTxLOONA/main/TrojanChuu.png"],
   [1394, 570, 1, "https://raw.githubusercontent.com/DEM0N194/VUTxLOONA/main/yinyang_noborder2.png"],
   [1670, 1585, 1, "https://raw.githubusercontent.com/DEM0N194/VUTxLOONA/main/ot12.png"],
+  [1168, 223, 1, "https://raw.githubusercontent.com/DEM0N194/VUTxLOONA/main/fitvut.png"],
 ]
 
 
